0ec54bea008c733b532fe7e48019bfaf70cac3f3
Facundo Recabarren <facundorecabarren10@gmail.com>
2024-02-23 17:02:03 -0300

    Paquetes listos, footer listo

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import { FaFacebookF, FaInstagram  } from "react-icons/fa6";
+import { BiLogoGmail } from "react-icons/bi";
 
 const Footer = () => {
   return (
-    <div></div>
+    <div className='px-10 pt-20'>
+      <section className='flex justify-between items-center py-5'>
+        <h2 className='font-extrabold text-xl text-[#023059]'>LOGO</h2>
+        <article className='flex flex-col justify-center items-center gap-5'>
+          <div className='flex justify-center items-center gap-3 h-6'>
+            <a className='rounded-full border-[#023059] border cursor-pointer p-2 hover:border-[#59342C] duration-300'>
+              <FaFacebookF className='text-[#023059] hover:text-[#59342C] duration-300'/>
+            </a>
+            <a className='rounded-full border-[#023059] border cursor-pointer p-2 hover:border-[#59342C] duration-300'>
+              <FaInstagram className='text-[#023059] hover:text-[#59342C] duration-300'/>
+            </a>
+            <a className='rounded-full border-[#023059] border cursor-pointer p-2 hover:border-[#59342C] duration-300'>
+              <BiLogoGmail className='text-[#023059] hover:text-[#59342C] duration-300'/>
+            </a>
+          </div>
+          <div className='flex justify-center items-center gap-3 text-[#023059] font-semibold text-sm'>
+            <a href="" className='hover:text-[#59342C] duration-300'>Inicio</a><a href="" className='hover:text-[#59342C] duration-300'>Servicios</a><a href="" className='hover:text-[#59342C] duration-300'>Nosotros</a><a href="" className='hover:text-[#59342C] duration-300'>Testimonios</a>
+          </div>
+        </article>
+      </section>
+      <hr />
+      <section className='py-5 flex flex-col justify-center items-center gap-3'>
+        <p className='text-sm font-medium opacity-80'>© 2024 Nombre. All Rights Reserved.</p>
+        <p className='font-bold text-[#023059]'>Made by Elizca</p>
+      </section>
+    </div>
   )
 }
 
--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -2,59 +2,30 @@ import React from 'react'
 
 const Services = () => {
   return (
-    <div className='pt-10'>
-      <div className='flex flex-col justify-center items-center gap-4'>
-        <h2 className='text-[#023059] text-4xl font-extrabold'>Nuestros Servicios</h2>
-        <h3 className="text-[#023059] opacity-80 font-semibold">Lorem ipsum dolor sit amet consectetur numquam reprehenderit error atque delectus dolore veniam ducimus.</h3>
-      </div>
-      <div className='flex justify-center items-center'>
-        <section className='w-1/2'>
-          <div className='relative'>
-            <img src="https://res.cloudinary.com/dreso9ye9/image/upload/v1708620014/vecteezy_3d-male-character-presenting-to-the-left_24785833_possff.png" alt="" className='w-[30rem]'/>
-            <img src="https://res.cloudinary.com/dreso9ye9/image/upload/v1708623767/Group_5_q1exqv.svg" alt="" className='absolute top-0 -z-10'/>
+    <div className='pt-20 relative'>
+      <img src="https://res.cloudinary.com/dreso9ye9/image/upload/v1708712106/Meteor_utmbba.svg" alt="" className='absolute -z-10 hidden sm:flex sm:h-[30rem] w-full'/>
+      <img src="https://res.cloudinary.com/dreso9ye9/image/upload/v1708711530/Meteor_1_zyru00.svg" alt="" className='sm:hidden absolute -z-10 h-[35rem]'/>
+      <img src="https://res.cloudinary.com/dreso9ye9/image/upload/v1708711530/Meteor_1_zyru00.svg" alt="" className='sm:hidden absolute -z-10 h-[35rem] bottom-10'/>
+      <div className='flex flex-col sm:flex sm:flex-row justify-center items-center gap-10 lg:gap-20 px-10'>
+        <section className='flex flex-col justify-center items-center gap-10 py-5 w-[22rem] sm:w-[30rem] shadow-md rounded-xl bg-white border-2 border-t-[2rem] border-[#023059]'>
+          <div className='flex flex-col justify-center items-center gap-6 pt-10'>
+            <div className='flex justify-center items-center w-40 h-40 bg-[url(https://res.cloudinary.com/dreso9ye9/image/upload/v1708656436/Agencia%20viajes/asset_5_jm3p3r.svg)] bg-contain bg-no-repeat bg-center'>
+              <img src="https://res.cloudinary.com/dreso9ye9/image/upload/v1708651267/Agencia%20viajes/vecteezy_3d-airplane-transparent-background-aircraft-aeroplane-3d_22950875_lqt46j.webp" alt=""/>
+            </div>
+            <h2 className='text-[#023059] font-bold text-2xl'>Viajes</h2>
+            <p className='px-12 text-center text-sm font-medium opacity-80'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus nesciunt cumque</p>
           </div>
+          <a href="" className='flex gap-2 justify-center items-center py-3 px-10 lg:px-6 text-lg text-[#023059] font-bold bg-[#F2CAA7] hover:bg-[#f2caa756] duration-500 rounded-xl'>Más información</a>
         </section>
-        <section className='grid grid-cols-2 gap-y-10 w-1/2'>
-        <article className='col-span-1 shadow-md rounded-xl py-5 w-[20rem]'>
-            <div className='flex flex-col justify-center items-center gap-3 w-full'>
-              <div className='relative'>
-                <img src="https://res.cloudinary.com/dreso9ye9/image/upload/v1708618865/vecteezy_3d-airplane-transparent-background-aircraft-aeroplane-3d_22950875_josr1n.png" alt="" className='w-20 z-10'/>
-                <img src="https://res.cloudinary.com/dreso9ye9/image/upload/v1708620777/Rectangle_3_1_np6ykh.svg" alt="" className='absolute w-10 top-0 right-0 -z-10'/>
-              </div>
-              <h2 className='text-lg font-bold text-[#59342C]'>Los mejores vuelos</h2>
-              <p className='text-center text-xs font-medium text-[#59342C] px-10'>Lorem ipsum dolor sit amet consectetur.</p>
+        <section className='flex flex-col justify-center items-center gap-10 py-5 w-[22rem] sm:w-[30rem] shadow-md rounded-xl bg-white border-2 border-t-[2rem] border-[#023059]'>
+          <div className='flex flex-col justify-center items-center gap-6 pt-10'>
+            <div className='flex justify-center items-center w-40 h-40 bg-[url(https://res.cloudinary.com/dreso9ye9/image/upload/v1708656436/Agencia%20viajes/asset_5_jm3p3r.svg)] bg-contain bg-no-repeat bg-center'>
+              <img src="https://res.cloudinary.com/dreso9ye9/image/upload/v1708651312/Agencia%20viajes/Group_5_2_l0wfe9.webp" alt=""/>
             </div>
-          </article>
-          <article className='col-span-1 shadow-md rounded-xl py-5 w-[20rem]'>
-            <div className='flex flex-col justify-center items-center gap-3 w-full'>
-              <div className='relative'>
-                <img src="https://res.cloudinary.com/dreso9ye9/image/upload/v1708618865/vecteezy_3d-airplane-transparent-background-aircraft-aeroplane-3d_22950875_josr1n.png" alt="" className='w-20 z-10'/>
-                <img src="https://res.cloudinary.com/dreso9ye9/image/upload/v1708620510/Rectangle_3_ppifuz.svg" alt="" className='absolute w-10 top-0 right-0 -z-10'/>
-              </div>
-              <h2 className='text-lg font-bold text-[#59342C]'>Los mejores vuelos</h2>
-              <p className='text-center text-xs font-medium text-[#59342C] px-10'>Lorem ipsum dolor sit amet consectetur.</p>
-            </div>
-          </article>
-          <article className='col-span-1 shadow-md rounded-xl py-5 w-[20rem]'>
-            <div className='flex flex-col justify-center items-center gap-3 w-full'>
-              <div className='relative'>
-                <img src="https://res.cloudinary.com/dreso9ye9/image/upload/v1708618865/vecteezy_3d-airplane-transparent-background-aircraft-aeroplane-3d_22950875_josr1n.png" alt="" className='w-20 z-10'/>
-                <img src="https://res.cloudinary.com/dreso9ye9/image/upload/v1708620510/Rectangle_3_ppifuz.svg" alt="" className='absolute w-10 top-0 right-0 -z-10'/>
-              </div>
-              <h2 className='text-lg font-bold text-[#59342C]'>Los mejores vuelos</h2>
-              <p className='text-center text-xs font-medium text-[#59342C] px-10'>Lorem ipsum dolor sit amet consectetur.</p>
-            </div>
-          </article>
-          <article className='col-span-1 shadow-md rounded-xl py-5 w-[20rem]'>
-            <div className='flex flex-col justify-center items-center gap-3 w-full'>
-              <div className='relative'>
-                <img src="https://res.cloudinary.com/dreso9ye9/image/upload/v1708618865/vecteezy_3d-airplane-transparent-background-aircraft-aeroplane-3d_22950875_josr1n.png" alt="" className='w-20 z-10'/>
-                <img src="https://res.cloudinary.com/dreso9ye9/image/upload/v1708620777/Rectangle_3_1_np6ykh.svg" alt="" className='absolute w-10 top-0 right-0 -z-10'/>
-              </div>
-              <h2 className='text-lg font-bold text-[#59342C]'>Los mejores vuelos</h2>
-              <p className='text-center text-xs font-medium text-[#59342C] px-10'>Lorem ipsum dolor sit amet consectetur.</p>
-            </div>
-          </article>
+            <h2 className='text-[#023059] font-bold text-2xl'>Excursiones</h2>
+            <p className='px-12 text-center text-sm font-medium opacity-80'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloribus nesciunt cumque</p>
+          </div>
+          <a href="" className='flex gap-2 justify-center items-center py-3 px-10 lg:px-6 text-lg text-[#023059] font-bold bg-[#F2CAA7] hover:bg-[#f2caa756] duration-500 rounded-xl'>Más información</a>
         </section>
       </div>
     </div>
