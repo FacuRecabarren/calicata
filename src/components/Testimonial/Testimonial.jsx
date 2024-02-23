9e9fa0f5fc8c87541490e5af73a7ea47166b09b3
Facundo Recabarren <facundorecabarren10@gmail.com>
2024-02-23 18:24:38 -0300

    Correccion css about

--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -8,7 +8,7 @@ const Testimonial = () => {
     
 
     return (
-        <div className=' pt-28 pb-10'>
+        <div className=' pt-20 pb-10'>
             <div className="flex flex-col justify-center items-center gap-4 pb-10">
                 <h2 className="text-[#023059] text-4xl font-extrabold">Reseñas</h2>
                 <h3 className="text-[#023059] opacity-80 font-semibold">Calificaciones de algunos de nuestros clientes</h3>
