6fa344a00ecfa11e9bdf44b61bea7895d7c405ca
juane <juane.elizondo@yahoo.com>
2024-03-04 11:47:05 -0300

    contacto conectado con emailjs

--- src/components/ContactUs/ContactUs.jsx
+++ src/components/ContactUs/ContactUs.jsx
@@ -1,16 +1,58 @@
-// import emailjs from '@emailjs/browser';
-// import { useState, useRef } from 'react';
+import emailjs from '@emailjs/browser';
+import { useState, useRef } from 'react';
 // import Aos from 'aos';
 // import 'aos/dist/aos.css';
 // import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
-// import Swal from 'sweetalert2';
+import Swal from 'sweetalert2';
 
 
 const ContactUs = () => {
 
   const { t, i18n } = useTranslation()
 
+  const [data, setData] = useState({
+    username: '',
+    email: '',
+    message: ''
+  })
+
+  const refForm = useRef();
+
+
+  const handleChange = (event) => {
+    setData({...data, [event.target.name]: event.target.value })
+  }
+
+  const messageSuccess = i18n.language == 'en' ?  'Your message has been successfully sent!' : '¡Su consulta ha sido enviada con éxito!'
+  const messageError = i18n.language == 'en' ?  'Your message could not be sent' : 'No se ha podido enviar su consulta'
+
+  const handleSubmit = (e) => {
+      e.preventDefault();
+
+      const serviceId = 'service_dbw3e5y'
+      const templateId = 'template_e71u7wj'
+      const publicKey = '94sDCibUuaXwmSlcV'
+
+      emailjs.sendForm(serviceId, templateId, refForm.current, publicKey)
+        .then((result) => {
+        Swal.fire({
+          title: messageSuccess,
+          icon: 'success',
+        })
+        console.log(result.text);
+        }, () => {
+          Swal.fire({
+            title: messageError,
+            icon: 'error',
+          })
+        });
+      setData({
+        username: '',
+        email: '',
+        message: ''
+      })
+  }
     
 
 
@@ -26,7 +68,7 @@ const ContactUs = () => {
             </h3>
           </div>
           <div>
-            <form className='w-[17rem] sm:w-[22rem] md:w-[30rem]'>
+          <form ref={refForm} onSubmit={handleSubmit} className='w-[17rem] sm:w-[22rem] md:w-[30rem]'>
               <div className="mb-4">
                 <label
                   htmlFor="username"
@@ -36,8 +78,10 @@ const ContactUs = () => {
                 </label>
                 <input
                   type="text"
-                  name="username"
+                  name='username'
                   id="username"
+                  value={data.username}
+                  onChange={handleChange}
                   className="form-input w-full px-4 py-2 border rounded-2xl text-gray-700 focus:ring-[#E8C368]"
                   required
                   placeholder={t("placeholderName")}
@@ -52,8 +96,10 @@ const ContactUs = () => {
                 </label>
                 <input
                   type="email"
-                  name="email"
+                  name='email'
                   id="email"
+                  value={data.email}
+                  onChange={handleChange}
                   className="form-input w-full px-4 py-2 border rounded-2xl text-gray-700 focus:ring-[#E8C368]"
                   required
                   placeholder={t("placeholderMail")}
@@ -67,8 +113,10 @@ const ContactUs = () => {
                   {t("message")}
                 </label>
                 <textarea
-                  name="message"
+                  name='message'
                   id="message"
+                  value={data.message}
+                  onChange={handleChange}
                   className="form-input w-full h-[9rem] p-2  border rounded-2xl text-gray-700 focus:ring-[#E8C368]"
                   placeholder={t("placeholderMessage")}
                   required
@@ -91,75 +139,65 @@ const ContactUs = () => {
 
 
 
-  {/* <div id='contact' className="h-full w-full">
-  <div className="flex flex-col justify-center items-center gap-4 py-10">
-    <h2 className="text-[#218B7D] text-4xl font-extrabold">{t("contact")}</h2>
-    <h3 className="text-[#218B7D] opacity-80 font-semibold text-center px-5 lg:text-start lg:px-0 font-lora italic">Consulta tus dudas y nosotros te ayudamos</h3>
-  </div>
-<section className='flex justify-center items-center mx-5 sm:mx-0'>
-<div className="bg-white px-8 py-8 rounded-lg shadow-lg max-w-sm lg:w-1/3 z-10">
-  <form>
-    <div className="mb-4">
-      <label
-        htmlFor="username"
-        className="block text-gray-700 text-sm font-semibold mb-2"
-      >
-        {t("name")}
-      </label>
-      <input
-        type="text"
-        name='username'
-        id="username"
-        className="form-input w-full px-4 py-2 border rounded-lg text-gray-700 focus:ring-[#E8C368]"
-        required
-        placeholder={t("placeholderName")}
-      />
-    </div>
-    <div className="mb-4">
-      <label
-        htmlFor="email"
-        className="block text-gray-700 text-sm font-semibold mb-2"
-      >
-        {t("email")}
-      </label>
-      <input
-        type="email"
-        name='email'
-        id="email"
-        
-        
-        className="form-input w-full px-4 py-2 border rounded-lg text-gray-700 focus:ring-[#E8C368]"
-        required
-        placeholder={t("placeholderMail")}
-      />
-    </div>
-    <div className="mb-6">
-      <label
-        htmlFor="message"
-        className="block text-gray-700 text-sm font-semibold mb-2"
-      >
-        {t("message")}
-      </label>
-      <textarea
-        name='message'
-        id="message"
-        
-        
-        className="form-input w-full p-2  border rounded-lg text-gray-700 focus:ring-[#E8C368]"
-        placeholder={t("placeholderMessage")}
-        required
-      />
-    </div>
-    <button
-      type="submit"
-      className="w-full font-bold text-white bg-[#218B7D] hover:bg-[#FE904D] px-4 py-2 rounded-lg duration-500 focus:outline-none focus:ring-2 focus:ring-[#E8C368] focus:ring-opacity-50"
-    >
-      {t("send")}
-    </button>
-  </form>
-</div>
-{/* <div>
-  <img src="https://res.cloudinary.com/dreso9ye9/image/upload/v1708909733/vecteezy_3d-male-character-jumping-out-from-computer-screen-and_24658878_r54dyf.webp" alt="" className='w-[30rem]'/>
-</div> */}
-// </section>
-// </div> */}
+  
+            // <form refForm={refform} onSubmit={handleSubmit} className='w-[17rem] sm:w-[22rem] md:w-[30rem]'>
+            //   <div className="mb-4">
+            //     <label
+            //       htmlFor="username"
+            //       className="block text-gray-700 text-sm font-semibold mb-2"
+            //     >
+            //       {t("name")}
+            //     </label>
+            //     <input
+            //       type="text"
+            //       name="username"
+            //       id="username"
+            //       className="form-input w-full px-4 py-2 border rounded-2xl text-gray-700 focus:ring-[#E8C368]"
+            //       required
+            //       placeholder={t("placeholderName")}
+            //       onChange={handleChange}
+            //       value={data.username}
+            //     />
+            //   </div>
+            //   <div className="mb-4">
+            //     <label
+            //       htmlFor="email"
+            //       className="block text-gray-700 text-sm font-semibold mb-2"
+            //     >
+            //       {t("email")}
+            //     </label>
+            //     <input
+            //       type="email"
+            //       name="email"
+            //       id="email"
+            //       className="form-input w-full px-4 py-2 border rounded-2xl text-gray-700 focus:ring-[#E8C368]"
+            //       required
+            //       placeholder={t("placeholderMail")}
+            //       onChange={handleChange}
+            //       value={data.email}
+            //     />
+            //   </div>
+            //   <div className="mb-6">
+            //     <label
+            //       htmlFor="message"
+            //       className="block text-gray-700 text-sm font-semibold mb-2"
+            //     >
+            //       {t("message")}
+            //     </label>
+            //     <textarea
+            //       name="message"
+            //       id="message"
+            //       className="form-input w-full h-[9rem] p-2  border rounded-2xl text-gray-700 focus:ring-[#E8C368]"
+            //       placeholder={t("placeholderMessage")}
+            //       required
+            //       onChange={handleChange}
+            //       value={data.message}
+            //     />
+            //   </div>
+            //   <button
+            //     type="submit"
+            //     className="w-full font-bold text-white bg-[#218B7D] hover:bg-[#FE904D] px-4 py-2 rounded-lg duration-500 focus:outline-none focus:ring-2 focus:ring-[#E8C368] focus:ring-opacity-50"
+            //   >
+            //     {t("send")}
+            //   </button>
+            // </form>
